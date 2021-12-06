fe84cb4edbc565bda1da08779fa7c08d59c2780c
Md. Rony <rony15-10527@diu.edu.bd>
2021-12-06 17:16:05 +0600

    This is my first Flask web App

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_world():
+    if request.method == "POST":
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+    allTodo = Todo.query.all()
+    return render_template('index.html', all_todo=allTodo)
+
+
+@app.route("/about")
+def about():
+    return "<h1>This is my first flask web application.  Basically, this is a To Do Application. This is made by me. </h1>"
+
+
+@app.route("/update")
+def update():
+    allTodo = Todo.query.all()
+    return "<p>This is my datbase</p>"
+
+
+@app.route("/delete/<int:sno>")
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
+
+
+@app.route("/about")
+def hello_Rony():
+    return "<p>This Md. Rony</p>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
